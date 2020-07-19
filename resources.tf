e15564820a852dc210954f33d866d1c2c3d3a8ee
Jeswanth Moravineni <jeswanth.moravineni@sap.com>
2020-07-19 19:50:34 +0200

    error free

--- resources.tf
+++ resources.tf
@@ -1,58 +1,58 @@
 #providers
 provider "aws" {
-	access_key = "${var.access_key}"
-	secret_key = "${var.secret_key}"
-	region = "${var.region}"
+	access_key = var.access_key
+	secret_key = var.secret_key
+	region = var.region
 }
 
 #resources
 resource "aws_vpc" "vpc" {
-  cidr_block = "${var.cidr_vpc}"
+  cidr_block = var.cidr_vpc
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags {
-    "Environment" = "${var.environment_tag}"
+  tags = {
+    "Environment" = var.environment_tag
   }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  tags {
-    "Environment" = "${var.environment_tag}"
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    "Environment" = var.environment_tag
   }
 }
 
 resource "aws_subnet" "subnet_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.cidr_subnet}"
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = var.cidr_subnet
   map_public_ip_on_launch = "true"
-  availability_zone = "${var.availability_zone}"
-  tags {
-    "Environment" = "${var.environment_tag}"
+  availability_zone = var.availability_zone
+  tags = {
+    "Environment" = var.environment_tag
   }
 }
 
 resource "aws_route_table" "rtb_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   route {
       cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.igw.id}"
+      gateway_id = aws_internet_gateway.igw.id
   }
 
-  tags {
-    "Environment" = "${var.environment_tag}"
+  tags = {
+    "Environment" = var.environment_tag
   }
 }
 
 resource "aws_route_table_association" "rta_subnet_public" {
-  subnet_id      = "${aws_subnet.subnet_public.id}"
-  route_table_id = "${aws_route_table.rtb_public.id}"
+  subnet_id      = aws_subnet.subnet_public.id
+  route_table_id = aws_route_table.rtb_public.id
 }
 
 resource "aws_security_group" "sg_22" {
   name = "sg_22"
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   # SSH access from the VPC
   ingress {
@@ -69,24 +69,24 @@ resource "aws_security_group" "sg_22" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags {
-    "Environment" = "${var.environment_tag}"
+  tags = {
+    "Environment" = var.environment_tag
   }
 }
 
 resource "aws_key_pair" "ec2key" {
   key_name = "publicKey"
-  public_key = "${file(var.public_key_path)}"
+  public_key = file(var.public_key_path)
 }
 
 resource "aws_instance" "testInstance" {
-  ami           = "${var.instance_ami}"
-  instance_type = "${var.instance_type}"
-  subnet_id = "${aws_subnet.subnet_public.id}"
-  vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
-  key_name = "${aws_key_pair.ec2key.key_name}"
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.subnet_public.id
+  vpc_security_group_ids = [aws_security_group.sg_22.id]
+  key_name = aws_key_pair.ec2key.key_name
 
-  tags {
-		"Environment" = "${var.environment_tag}"
+  tags = {
+		"Environment" = var.environment_tag
 	}
 }
